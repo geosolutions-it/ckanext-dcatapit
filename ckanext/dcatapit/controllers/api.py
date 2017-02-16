4a4012001ad147b423c0d1486bed4b4704473d7a
Tobia Di Pisa <tobia.dipisa@geo-solutions.it>
2017-02-16 13:11:55 +0100

    More test cases (#42)
    * Adding more test cases
    
    * Removing some tabs
    
    * Removing some tabs

--- ckanext/dcatapit/controllers/api.py
+++ ckanext/dcatapit/controllers/api.py
@@ -6,7 +6,7 @@ import ckan.model as model
 import ckan.logic as logic
 
 from ckan.controllers.api import ApiController
-from ckan.common import _, c, request, response
+from ckan.common import c, request
 
 log = logging.getLogger(__file__)
 
@@ -15,27 +15,28 @@ get_action = logic.get_action
 
 class DCATAPITApiController(ApiController):
 
-	def vocabulary_autocomplete(self):
-		q = request.str_params.get('incomplete', '')
-		q = unicode(urllib.unquote(q), 'utf-8')
+    def vocabulary_autocomplete(self):
+        q = request.str_params.get('incomplete', '')
+        q = unicode(urllib.unquote(q), 'utf-8')
 
-		vocab = request.params.get('vocabulary_id', None)
-		vocab = str(vocab)
+        vocab = request.params.get('vocabulary_id', None)
 
-		log.debug('Looking for Vocab %r', vocab)
+        vocab = str(vocab)
 
-		limit = request.params.get('limit', 10)
-		
-		tag_names = []
-		if q:
-			context = {'model': model, 'session': model.Session, 'user': c.user, 'auth_user_obj': c.userobj}
-			data_dict = {'q': q, 'limit': limit, 'vocabulary_id': vocab}
-			tag_names = get_action('tag_autocomplete')(context, data_dict)
+        log.debug('Looking for Vocab %r', vocab)
 
-		resultSet = {
-			'ResultSet': {
-				'Result': [{'Name': tag} for tag in tag_names]
-			}
-		}
+        limit = request.params.get('limit', 10)
 
-		return super(DCATAPITApiController, self)._finish_ok(resultSet)
+        tag_names = []
+        if q:
+            context = {'model': model, 'session': model.Session, 'user': c.user, 'auth_user_obj': c.userobj}
+            data_dict = {'q': q, 'limit': limit, 'vocabulary_id': vocab}
+            tag_names = get_action('tag_autocomplete')(context, data_dict)
+
+        resultSet = {
+            'ResultSet': {
+                'Result': [{'Name': tag} for tag in tag_names]
+            }
+        }
+
+        return super(DCATAPITApiController, self)._finish_ok(resultSet)
