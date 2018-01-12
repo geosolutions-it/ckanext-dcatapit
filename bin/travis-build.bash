fcbf3e6fd3cf373e6b1a9c8d92a16c6a1402f569
Cezary Statkiewicz <cezio@thelirium.net>
2018-01-12 10:56:14 +0100

    add multivalue fields to solr schema in travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -41,11 +41,11 @@ echo
 echo "Setting up Solr..."
 printf "NO_START=0\nJETTY_HOST=127.0.0.1\nJETTY_PORT=8983\nJAVA_HOME=$JAVA_HOME" | sudo tee /etc/default/jetty
 sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
-sudo sed -i -e 's-</fields>-<field name="dcat_theme" type="string" indexed="true" stored="false" multiValued="true"/></fields>-g' /etc/solr/conf/schema.xml
-sudo sed -i -e 's-</fields>-<field name="dcat_subtheme" type="string" indexed="true" stored="false" multiValued="true"/></fields>-g' /etc/solr/conf/schema.xml
-sudo sed -i -e 's-</fields>-<field name="dcat_subtheme_*" type="string" indexed="true" stored="false" multiValued="true"/></fields>-g' /etc/solr/conf/schema.xml
-sudo sed -i -e 's-</fields>-<field name="organization_region_*" type="string" indexed="true" stored="false" multiValued="true"/></fields>-g' /etc/solr/conf/schema.xml
-sudo sed -i -e 's-</fields>-<field name="resource_license_*" type="string" indexed="true" stored="false" multiValued="true"/></fields>-g' /etc/solr/conf/schema.xml
+sudo sed -i -e 's-</fields>-<field name="dcat_theme" type="string" indexed="true" stored="false" multiValued="true"/>\n</fields>-g' /etc/solr/conf/schema.xml
+sudo sed -i -e 's-</fields>-<field name="dcat_subtheme" type="string" indexed="true" stored="false" multiValued="true"/>\n</fields>-g' /etc/solr/conf/schema.xml
+sudo sed -i -e 's-</fields>-<field name="dcat_subtheme_*" type="string" indexed="true" stored="false" multiValued="true"/>\n</fields>-g' /etc/solr/conf/schema.xml
+sudo sed -i -e 's-</fields>-<field name="organization_region_*" type="string" indexed="true" stored="false" multiValued="true"/>\n</fields>-g' /etc/solr/conf/schema.xml
+sudo sed -i -e 's-</fields>-<field name="resource_license_*" type="string" indexed="true" stored="false" multiValued="true"/>\n</fields>-g' /etc/solr/conf/schema.xml
 grep '<field name=' /etc/solr/conf/schema.xml
 
 sudo service jetty restart
