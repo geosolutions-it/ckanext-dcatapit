c5f6e92c2d09b46c62e6b32e32898eebb069d296
Cezary Statkiewicz <cezio@thelirium.net>
2018-05-24 16:46:01 +0200

    conf var to disable form tabs #190

--- ckanext/dcatapit/plugin.py
+++ ckanext/dcatapit/plugin.py
@@ -276,6 +276,7 @@ class DCATAPITPackagePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm,
             'get_dcatapit_subthemes': helpers.get_dcatapit_subthemes,
             'dump_dcatapit_subthemes': helpers.dump_dcatapit_subthemes,
             'get_localized_subtheme': helpers.get_localized_subtheme,
+            'dcatapit_enable_form_tabs': helpers.get_enable_form_tabs,
         }
 
         if MLR:
